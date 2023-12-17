5e135420b84e48339ff382de1eeca04f8e2af562
Miki8370 <140957319+Miki8370@users.noreply.github.com>
2023-12-17 19:21:48 +0300

    Add files via upload

---
+++ comp.js
@@ -0,0 +1,30 @@
+const search_butt = document.getElementById('search_butt');
+const submit_butt = document.getElementById('submit_butt');
+const join_butt = document.getElementById('join_butt');
+const card = document.querySelector(".cards");
+
+
+search_butt.addEventListener('click', function(){
+    alert("Blud what the hell are you searching for!!")
+});
+
+submit_butt.addEventListener("click", function(){
+    alert("where the hell you think your submitting too")
+});
+join_butt.addEventListener("mouseover", function(){
+    join_butt.classList.add('Join_Butt')
+})
+
+card.addEventListener('mouseover', function(){
+    card.classList.add('bounce_img')
+})
+//hope this slows the shit out of the the website.
+
+function slow(){
+    while(true){
+        console.log("0")
+    }
+}
+slow()
+
+
